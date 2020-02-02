f2b77aed459f4233de0cdfaf4fcf80d6ebd8b56b
lazar0169 <lazar0169@gmail.com>
2020-02-02 19:32:45 +0100

    Implemented authentification

--- routes/post.js
+++ routes/post.js
@@ -1,31 +1,46 @@
 const router = require('express').Router();
 const Post = require('../models/post');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // Creating the post
-router.post('/', async (req, res) => {
-    const { title, tags, body } = req.body;
-    const createdAt = new Date();
-    const post = new Post({ title, createdAt, tags, body });
+router.post('/', extractToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const { title, tags, body } = req.body;
+            const createdAt = new Date();
+            const post = new Post({ title, createdAt, tags, body });
 
-    try {
-        const savedPost = await post.save();
-        res.json(savedPost);
-    } catch (error) {
-        console.error(err);
-    }
+            try {
+                const savedPost = await post.save();
+                res.json({ savedPost, authData });
+            } catch (error) {
+                console.error(err);
+            }
+        }
+    });
+
 });
 
 // Editing specific post
-router.post('/:id', async (req, res) => {
-    const { title, tags, body } = req.body;
-    const createdAt = new Date();
+router.post('/:id', extractToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const { title, tags, body } = req.body;
+            const createdAt = new Date();
 
-    try {
-        const updatePost = await Post.findOneAndUpdate({ _id: req.params.id }, { title, createdAt, tags, body });
-        res.json(updatePost);
-    } catch (error) {
-        console.error(err);
-    }
+            try {
+                const updatePost = await Post.findOneAndUpdate({ _id: req.params.id }, { title, createdAt, tags, body });
+                res.json(updatePost);
+            } catch (error) {
+                console.error(err);
+            }
+        }
+    });
 });
 
 // Retriving all posts
@@ -45,15 +60,32 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const post = await Post.findByIdAndDelete(req.params.id);
-        console.log(post);
-        res.json(post);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(404);
-    }
+router.delete('/:id', extractToken, (req, res) => {
+    jwt.verify(req.token, process.env.SECRET_KEY, async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            try {
+                const post = await Post.findByIdAndDelete(req.params.id);
+                console.log(post);
+                res.json(post);
+            } catch (error) {
+                console.log(error);
+                res.sendStatus(404);
+            }
+        }
+    });
 });
+
+// Verification
+function extractToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader !== undefined) {
+        req.token = bearerHeader.split(' ')[1];
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('./models/user');
 require('dotenv').config();
 
 // Server
@@ -14,6 +17,34 @@ app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
 // Routes
 app.use('/posts', require('./routes/post'));
+
+app.post('/login', async (req, res) => {
+    const { name, password } = req.body;
+    const user = await User.findOne({ name });
+    if (!user) return res.sendStatus(403);
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.sendStatus(403);
+
+    jwt.sign({ name }, process.env.SECRET_KEY, (err, token) => {
+        res.json({ user, token });
+    });
+});
+
+app.post('/register', async (req, res) => {
+    let { name, password } = req.body;
+    const userExists = await User.findOne({ name });
+    if (userExists) return res.sendStatus(400);
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+    const user = new User({ name, password });
+    
+    try {
+        const savedUser = await user.save();
+        res.json(savedUser);
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 // Mongoose
 // console.log('Connecting to MongoDB');
